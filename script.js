d77f71e940db41906a198d95d6b9a18acb468c16
Laksh Gupta <lakshgupta253@gmail.com>
2021-06-26 16:37:05 +0530

    Updated styling and urls reqd

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace("w3-black", "w3-blue");
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace("w3-blue", "w3-black");
+  }
+}
