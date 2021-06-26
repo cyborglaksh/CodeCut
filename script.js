b3b5a27c873c1dfa8c18966ae9a350e29448b78b
Anushka Gupta <anushkagupta645@gmail.com>
2021-06-26 17:18:37 +0530

    error in extention removed

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+let x=document.getElementById("list1")
+let z=document.getElementById("list2")
+let listPanel1=document.getElementById("listPanel1")
+let listPanel2=document.getElementById("listPanel2")
 
-
-function myFunction(id) {
-  var x = document.getElementById(id);
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-    x.previousElementSibling.className = 
-    x.previousElementSibling.className.replace("w3-black", "w3-blue");
+function myFunction(y){
+  if (y.className.indexOf("w3-show") == -1) {
+    y.className += " w3-show";
+    y.previousElementSibling.className = 
+    y.previousElementSibling.className.replace("w3-black", "w3-blue");
   } else { 
-    x.className = x.className.replace(" w3-show", "");
-    x.previousElementSibling.className = 
-    x.previousElementSibling.className.replace("w3-blue", "w3-black");
+    y.className = y.className.replace(" w3-show", "");
+    y.previousElementSibling.className = 
+    y.previousElementSibling.className.replace("w3-blue", "w3-black");
   }
+
 }
+listPanel1.addEventListener("click",()=>{
+  myFunction(x)
+  
+})
+listPanel2.addEventListener("click",()=>{
+  myFunction(z)
+})
