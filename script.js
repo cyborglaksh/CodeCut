12cbd7f8fef27814d046e956c74828c12358e621
Anushka Gupta <anushkagupta645@gmail.com>
2021-06-26 18:14:54 +0530

    accordion added

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-let x=document.getElementById("list1")
-let z=document.getElementById("list2")
-let listPanel1=document.getElementById("listPanel1")
-let listPanel2=document.getElementById("listPanel2")
+// let x=document.getElementById("list1")
+// let z=document.getElementById("list2")
+// let listPanel1=document.getElementById("listPanel1")
+// let listPanel2=document.getElementById("listPanel2")
 
-function myFunction(y){
-  if (y.className.indexOf("w3-show") == -1) {
-    y.className += " w3-show";
-    y.previousElementSibling.className = 
-    y.previousElementSibling.className.replace("w3-black", "w3-blue");
-  } else { 
-    y.className = y.className.replace(" w3-show", "");
-    y.previousElementSibling.className = 
-    y.previousElementSibling.className.replace("w3-blue", "w3-black");
-  }
+// function myFunction(y){
+//   if (y.className.indexOf("w3-show") == -1) {
+//     y.className += " w3-show";
+//     y.previousElementSibling.className = 
+//     y.previousElementSibling.className.replace("w3-black", "w3-blue");
+//   } else { 
+//     y.className = y.className.replace(" w3-show", "");
+//     y.previousElementSibling.className = 
+//     y.previousElementSibling.className.replace("w3-blue", "w3-black");
+//   }
 
-}
-listPanel1.addEventListener("click",()=>{
-  myFunction(x)
+// }
+// listPanel1.addEventListener("click",()=>{
+//   myFunction(x)
   
-})
-listPanel2.addEventListener("click",()=>{
-  myFunction(z)
-})
+// })
+// listPanel2.addEventListener("click",()=>{
+//   myFunction(z)
+// })
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
