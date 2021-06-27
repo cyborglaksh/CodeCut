798680f0fa05e4ae78b832bf1bd6522706b9cf8e
Laksh Gupta <lakshgupta253@gmail.com>
2021-06-27 13:08:21 +0530

    panels added in accordion

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-// let x=document.getElementById("list1")
-// let z=document.getElementById("list2")
-// let listPanel1=document.getElementById("listPanel1")
-// let listPanel2=document.getElementById("listPanel2")
-
-// function myFunction(y){
-//   if (y.className.indexOf("w3-show") == -1) {
-//     y.className += " w3-show";
-//     y.previousElementSibling.className = 
-//     y.previousElementSibling.className.replace("w3-black", "w3-blue");
-//   } else { 
-//     y.className = y.className.replace(" w3-show", "");
-//     y.previousElementSibling.className = 
-//     y.previousElementSibling.className.replace("w3-blue", "w3-black");
-//   }
-
-// }
-// listPanel1.addEventListener("click",()=>{
-//   myFunction(x)
-  
-// })
-// listPanel2.addEventListener("click",()=>{
-//   myFunction(z)
-// })
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
+let links=[]
+let delBtn=document.getElementById("deleteBtn")
+let listContainer=document.getElementById("listUrl")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("links") )
+const tabBtn = document.getElementById("tabBtn")
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
@@ -35,4 +16,46 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
+}
+
+if (leadsFromLocalStorage) {
+    links = leadsFromLocalStorage
+    render(links)
+}
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        links.push({"title":tabs[0].title,"url":tabs[0].url,"icon":tabs[0].favIconUrl})
+        localStorage.setItem("links", JSON.stringify(links) )
+        render(links)
+    })
+})
+
+delBtn.addEventListener("click", function() {
+    localStorage.clear()
+    links = []
+    render(links)
+})
+
+function render(listOFlinks){
+  let savedTabs=document.getElementById("savedTabs")
+  // savedTabs.innerHTML=`<a>${listOFlinks[0].title}</a>`
+    let listItems = ""
+    for (let i = 0; i < listOFlinks.length; i++) {
+        listItems += `
+            <div>
+                <a class="urlLink" target='_blank' href='${listOFlinks[i].url}'>
+                <img src="pin_PNG54.png.crdownload" alt="" class="service-icon">
+                    ${listOFlinks[i].title}
+                </a>
+            </div>
+        `
+        // let linkBlock=document.createElement("div")
+        // let link=document.createElement("a")
+        // link.setAttribute("href",`${listOFlinks[i].url}`)
+        // linkBlock.appendChild(link)
+        // savedTabs.appendChild(linkBlock)
+
+    }
+    savedTabs.innerHTML = listItems
 }
